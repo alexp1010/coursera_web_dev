0a454a6e6d1f223752d56a2ad0595fef090185d3
alexp1010 <alex.y.palamarchuk@gmail.com>
2017-01-28 14:30:23 +0200

    small corrections

--- module4-solution/script.js
+++ module4-solution/script.js
@@ -58,7 +58,7 @@ WARNING!!! WARNING!!!
     // 'j'. If the same, call byeSpeaker's 'speak' method with the current name
     // in the loop. Otherwise, call helloSpeaker's 'speak' method with the current
     // name in the loop.
-    if (firstLetter == 'j'/* fill in condition here */) {
+    if (firstLetter === "j"/* fill in condition here */) {
       byeSpeaker.speak(names[index]);
     } else {
       helloSpeaker.speak(names[index]);
